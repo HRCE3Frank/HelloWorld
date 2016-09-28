4803f80b597e00da55dbfe13408fbde3c1f8bf8e
Frank 'Shadownd' Exel <129062@hondsrugcollege.nl>
2016-09-28 15:30:51 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from User import User
 Jelte = User("Jelte", "1234", "129329@hondsrugcollege.nl")
 Matthijs = User("Matthijs", "4321", "129472@hondsrugcollege.nl")
 Jesper = User("Jesper", "0000", "129175@hondsrugcollege.nl")
-
+Jelte2 = User("Faggot", "6666", "DikkePik@pornhub.com")
 print(Jelte)
 print(Matthijs)
 print(Jesper)
